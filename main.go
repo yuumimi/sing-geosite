2a17889a0ce6f6a7808383bf56b859fb98b98123
caocaocc <anonymous@users.noreply.github.com>
2023-12-16 21:43:15 +0800

    Use Loyalsoldier/domain-list-custom

--- main.go
+++ main.go
@@ -58,10 +58,10 @@ func get(downloadURL *string) ([]byte, error) {
 
 func download(release *github.RepositoryRelease) ([]byte, error) {
 	geositeAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat"
+		return *it.Name == "geosite.dat"
 	})
 	geositeChecksumAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat.sha256sum"
+		return *it.Name == "geosite.dat.sha256sum"
 	})
 	if geositeAsset == nil {
 		return nil, E.New("geosite asset not found in upstream release ", release.Name)
@@ -271,8 +271,8 @@ func release(source string, destination string, output string, cnOutput string, 
 
 func main() {
 	err := release(
-		"v2fly/domain-list-community",
-		"sagernet/sing-geosite",
+		"Loyalsoldier/domain-list-custom",
+		"yuumimi/sing-geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
